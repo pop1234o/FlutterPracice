e96841cbd8db72ea01faf683d72add977b0d9685
pop1234o <859405648@qq.com>
2024-02-04 17:53:12 +0800

    feat:添加了知识

---
+++ lib/dart_third.dart
@@ -0,0 +1,264 @@
+
+//=====================类& 对象=============================
+
+
+void a17(){
+  //Some classes provide constant constructors.
+  // To create a compile-time constant using a constant constructor,
+  // put the const keyword before the constructor name:
+  var a = const ImmutablePoint(1, 1);
+  var b = const ImmutablePoint(1, 1);
+  assert(identical(a, b)); // They are the same instance!
+
+  var a1 = const ImmutablePoint(1, 1); // Creates a constant
+  var b1 = ImmutablePoint(1, 1); // Does NOT create a constant
+
+  assert(!identical(a1, b1)); // NOT the same instance!
+
+
+
+}
+
+class ImmutablePoint {
+  const ImmutablePoint(int i, int j);
+}
+
+
+//Instance variables can be final, in which case they must be set exactly once
+class ProfileMark {
+  final String name;
+  final DateTime start = DateTime.now();
+
+  ProfileMark(this.name);
+  ProfileMark.unnamed() : name = '';
+}
+
+//每个类都是隐式接口。
+
+//Static variables Static methods
+class Queue {
+  static const initialCapacity = 16;
+// ···
+}
+
+//=====构造方法
+
+class Vector2d {
+  final double x;
+  final double y;
+  Vector2d.named({required this.x, required this.y});
+}
+
+class Vector3d extends Vector2d {
+  final double z;
+  // Forward the y parameter to the named super constructor like:
+  // 等于
+  // Vector3d.yzPlane({required double y, required this.z})
+  //       : super.named(x: 0, y: y);
+  Vector3d.yzPlane({required super.y, required this.z}) : super.named(x: 0);
+}
+
+
+
+//Besides invoking a superclass constructor, you can also initialize instance variables
+// before the constructor body runs. Separate initializers with commas.
+
+// Initializer list sets instance variables before the constructor body runs.
+//初始化列表，before the constructor body runs.
+// Point.fromJson(Map<String, double> json)
+//     : x = json['x']!,
+// y = json['y']! {
+// print('In Point.fromJson(): ($x, $y)');
+// }
+
+
+//this
+class Point {
+  double x, y;
+
+  // The main constructor for this class.
+  Point(this.x, this.y);
+
+  // Delegates to the main constructor.
+  Point.alongXAxis(double x) : this(x, 0);
+}
+
+
+
+//Constant constructors
+//If your class produces objects that never change, you can make these objects compile-time constants.
+// To do this, define a const constructor and make sure that all instance variables are final.
+class ImmutablePoint1 {
+  static const ImmutablePoint1 origin = ImmutablePoint1(0, 0);
+  //都是final
+  final double x, y;
+
+  const ImmutablePoint1(this.x, this.y);
+}
+
+
+//Use the factory keyword when implementing a constructor that doesn’t always create a new instance of its class.
+// factory构造函数，允许不创建新的对象，返回你要返回的对象。
+class Logger {
+  final String name;
+  bool mute = false;
+
+  // _cache is library-private, thanks to
+  // the _ in front of its name.
+  static final Map<String, Logger> _cache = <String, Logger>{};
+
+  factory Logger(String name) {
+    return _cache.putIfAbsent(name, () => Logger._internal(name));
+  }
+
+  factory Logger.fromJson(Map<String, Object> json) {
+    return Logger(json['name'].toString());
+  }
+
+  Logger._internal(this.name);
+
+  void log(String msg) {
+    if (!mute) print(msg);
+  }
+}
+
+//====================成员方法================
+
+
+
+
+//定义operator
+class Vector {
+  final int x, y;
+
+  Vector(this.x, this.y);
+
+  Vector operator +(Vector v) => Vector(x + v.x, y + v.y);
+  Vector operator -(Vector v) => Vector(x - v.x, y - v.y);
+
+  @override
+  bool operator ==(Object other) =>
+      other is Vector && x == other.x && y == other.y;
+
+  @override
+  int get hashCode => Object.hash(x, y);
+}
+
+void main() {
+  final v = Vector(2, 3);
+  final w = Vector(2, 2);
+
+  assert(v + w == Vector(4, 5));
+  assert(v - w == Vector(0, 1));
+}
+
+//getter setter
+class Rectangle {
+  double left, top, width, height;
+
+  Rectangle(this.left, this.top, this.width, this.height);
+
+  // Define two calculated properties: right and bottom.
+  double get right => left + width;
+  set right(double value) => left = value - width;
+  double get bottom => top + height;
+  set bottom(double value) => top = value - height;
+}
+void main1() {
+  var rect = Rectangle(3, 4, 20, 15);
+
+  assert(rect.left == 3);
+  rect.right = 12;// setter 相当于执行的语句，否则你还是要定义一个方法
+  assert(rect.left == -8);
+}
+
+//混合继承  Mixins
+
+
+//=================拓展方法=================
+extension NumberParsing on String {
+  int parseInt() {
+    return int.parse(this);
+  }
+// ···
+}
+
+a18(){
+  "11".parseInt();
+
+  //You can’t invoke extension methods on variables of type dynamic. F
+  //or example, the following code results in a runtime exception:
+  dynamic d = '2';
+  print(d.parseInt()); // Runtime exception: NoSuchMethodError
+}
+
+//You can invoke an unnamed extension’s static members only within the extension declaration.
+//只能在定义处调用
+extension on String {
+  bool get isBlank => trim().isEmpty;
+}
+
+//call  objects  类（）   执行的方法 叫call
+class WannabeFunction {
+  String call(String a, String b, String c) => '$a $b $c!';
+}
+void a19(){
+  var wf = WannabeFunction();
+  var out = wf('Hi', 'there,', 'gang');
+  void main() => print(out);
+}
+
+//base 不允许 mixin  和implements
+// To enforce inheritance of a class or mixin’s implementation,
+// use the base modifier. A base class disallows implementation outside of its own library. T
+
+
+//https://dart.cn/language/modifier-reference
+// Declaration	Construct?	Extend?	Implement?	Mix in?	Exhaustive? (穷举)
+// class	Yes	Yes	Yes	No	No
+// base class	Yes	Yes	No	No	No
+// interface class	Yes	No	Yes	No	No
+// final class	Yes	No	No	No	No
+// sealed class	No	No	No	No	Yes
+// abstract class	No	Yes	Yes	No	No
+// abstract base class	No	Yes	No	No	No
+// abstract interface class	No	No	Yes	No	No
+// abstract final class	No	No	No	No	No
+// mixin class	Yes	Yes	Yes	Yes	No
+// base mixin class	Yes	Yes	No	Yes	No
+// abstract mixin class	No	Yes	Yes	Yes	No
+// abstract base mixin class	No	Yes	No	Yes	No
+// mixin	No	No	Yes	Yes	No
+// base mixin	No	No	No	Yes	No
+
+//====================异步编程=======
+// 消耗时间的，写法和同步一样。
+
+Future<void> checkVersion() async {
+  var version = await lookUpVersion();
+  // Do something with version
+}
+
+lookUpVersion() {
+}
+
+// await for  直到循环结束，或者break
+void main3() async {
+  // ...
+  await for (final request in requestServer) {
+    handleRequest(request);
+  }
+  // ...
+}
+
+void handleRequest(request) {
+}
+
+//异步场景包括调用系统 API，例如非阻塞的 I/O 操作、HTTP 请求或与浏览器交互。
+// 还有一些场景是利用 Dart 的 isolate 进行计算，或等待一个计时器的触发。这些场景要么是在不同的线程运行，
+// 要么是被系统或 Dart 运行时处理，让 Dart 代码可以在计算时同步运行
+
+
+
+
+
---
+++ lib/flutter/flutter_main.dart
@@ -0,0 +1,108 @@
+
+
+
+
+//StatelessWidget
+//
+
+
+//您可能想知道为什么StatefulWidget和State是单独的对象。在Flutter中，这两类对象有不同的生命周期。
+// Widgets是临时对象，用于构造应用程序当前状态的表示。State另一方面，对象在调用之间是持久的 build()，允许它们记住信息。
+
+
+//控件逻辑独立封装
+
+//定义callback typedef CartChangedCallback = Function(Product product, bool inCart);
+
+
+
+//state 中 weight获取
+
+//，如果要添加 padding、margin、边框或背景颜色，你就可以用上 Container 了。
+
+
+//所有布局 widgets 都具有以下任一项：
+//
+//一个 child 属性，如果它们只包含一个子项 —— 例如 Center 和 Container
+//
+//一个 children 属性，如果它们包含多个子项 —— 例如 Row、Column、ListView 和 Stack
+
+//一个 Flutter app 本身就是一个 widget，大多数 widgets 都有一个 build() 方法，在 app 的 build() 方法中实例化和返回一个 widget 会让它显示出来。
+
+
+
+//在 Flutter 中创建行或列，可以将子 widgets 列表添加到 Row 或 Column widget 中
+//通过使用 Expanded widget，可以调整 widgets 的大小以适合行或列。
+
+//一个个控件组合的写法
+// final leftColumn = Container(
+//   padding: const EdgeInsets.fromLTRB(20, 30, 20, 20),
+//   child: Column(
+//     children: [
+//       titleText,
+//       subTitle,
+//       ratings,
+//       iconList,
+//     ],
+//   ),
+// );
+
+
+//=============container
+//增加 padding、margins、borders
+//改变背景色或者图片
+//只包含一个子 widget，但是这个子 widget 可以是行、列或者是 widget 树的根 widget
+//Container 还用来为每个图片添加圆角和外边距：
+
+//============GridView
+//==============Stack 多个控件叠加。
+
+//===============Card
+// 默认情况下，Card 的大小是 0x0 像素。你可以使用 SizedBox 控制 card 的大小。
+//================== ListTile 用来做item
+//它可以很轻松的创建一个包含三行文本以及可选的行前和行尾图标的行。 ListTile 在 Card 或者 ListView 中最常用，但是也可以在别处使用
+
+
+
+
+
+
+// android 与 widget
+//widget 和 View 还是有一些差异。首先，widget 有着不一样的生命周期：它们是不可变的，一旦需要变化则生命周期终止。
+// 任何时候 widget 或它们的状态变化时， Flutter 框架都会创建一个新的 widget 树的实例。
+// 对比来看，一个 Android View 只会绘制一次，除非调用 invalidate 才会重绘。
+
+
+// Text Widget 是一个普通的 StatelessWidget,假如你想要动态地改变 “I Like Flutter”，例如当你点击一个 FloatingActionButton 的时候，该怎么办呢？
+// 为了实现这个效果，将 Text Widget 嵌入一个 StatefulWidget 中
+
+
+//如何在布局中添加或删除一个组件？ 返回不同的组件
+
+
+//在 Flutter 中你需要使用 Navigator 和 Route 在同一个 Activity 内的不同界面间进行跳转。
+// void main() {
+//   runApp(MaterialApp(
+//     home: const MyAppHome(), // Becomes the route named '/'.
+//     routes: <String, WidgetBuilder>{
+//       '/a': (context) => const MyPage(title: 'page A'),
+//       '/b': (context) => const MyPage(title: 'page B'),
+//       '/c': (context) => const MyPage(title: 'page C'),
+//     },
+//   ));
+// }
+
+// Navigator.of(context).pushNamed('/b');
+
+
+
+
+
+
+
+
+
+
+
+
+
